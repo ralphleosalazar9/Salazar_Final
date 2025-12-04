4d10fa28eaf222a4fe1c61051cc2af8bb6d0a28c
ralphleosalazar9 <ralphleosalazar9@gmail.com>
2025-12-04 09:51:00 +0800

    Add files via upload

---
+++ Salazar3/Salazar3.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Salazar3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Salazar3
+{
+    public static void main (String [] args) {
+        
+        for (int r = 1; r <= 5; r++)
+        {
+            for (int s = 1; s <= r; s++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
